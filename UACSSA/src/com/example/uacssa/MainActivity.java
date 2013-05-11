3e3c8a6f0228cf4e9747b10c7a783cf68b9677a0
Yuwe DUan <yuwei@ualberta.ca>
2013-05-10 23:27:44 -0600

    change

--- UACSSA/src/com/example/uacssa/MainActivity.java
+++ UACSSA/src/com/example/uacssa/MainActivity.java
@@ -1,15 +1,44 @@
 package com.example.uacssa;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
+/**
+ *
+ */
 public class MainActivity extends Activity {
-
-	@Override
+	private Button bbs,map;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		bbs = (Button) findViewById(R.id.button1);
+		map = (Button) findViewById(R.id.button2);
+		bbs.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent tobbs = new Intent();
+				tobbs.setClass(MainActivity.this, BbsBrowserActivity.class);
+				startActivity(tobbs);
+			}
+		});
+		map.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent tobbs = new Intent();
+				tobbs.setClass(MainActivity.this, MapActivity.class);
+				startActivity(tobbs);
+			}
+		});
+		
 	}
 
 	@Override
